88b4130202e8df0ad4c76ddedcda326985bc167a
HelenEVA <92659299+HelenEVA@users.noreply.github.com>
2022-02-17 01:48:45 +0700

    TestMaxSpeed

--- src/test/java/Lesson18Test.java
+++ src/test/java/Lesson18Test.java
@@ -19,5 +19,18 @@ public class Lesson18Test {
 
     }
 
+    @Test
+
+    public void shouldMaxSpeedRetired(){
+
+        int[]speeds = {1, 0, 15};
+
+        int [] expected = {15};
+        int [] actual = Utils.getSpeed(speeds);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
 
 }
