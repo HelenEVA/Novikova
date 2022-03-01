07aa4a10709cdf558b776dd02b5305abd6fabd0a
HelenEVA <92659299+HelenEVA@users.noreply.github.com>
2022-03-01 22:33:03 +0700

    editing maven settings, adding tests

--- src/test/java/Lesson18Test.java
+++ src/test/java/Lesson18Test.java
@@ -4,16 +4,15 @@ import org.junit.jupiter.api.Test;
 public class Lesson18Test {
 
 
-
     @Test
 
-    public void shouldIsGreenLightTru(){
+    public void shouldIsGreenLightTru() {
 
-        Utils.isGreenLight=true;
-        int[]speeds = {1, 0, 15};
+        Utils.isGreenLight = true;
+        int[] speeds = {1, 0, 15};
 
-        int [] expected = {};
-        int [] actual = Utils.getSpeed(speeds);
+        int[] expected = {};
+        int[] actual = Utils.getSpeed(speeds);
 
         Assertions.assertArrayEquals(expected, actual);
 
@@ -21,12 +20,88 @@ public class Lesson18Test {
 
     @Test
 
-    public void shouldMaxSpeedRetired(){
+    public void shouldIsGreenLightFalse() {
 
-        int[]speeds = {1, 0, 15};
+        Utils.isGreenLight = false;
+        int[] speeds = {1, 0, 15};
 
-        int [] expected = {15};
-        int [] actual = Utils.getSpeed(speeds);
+        int[] expected = {15};
+        int[] actual = Utils.getSpeed(speeds);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+
+    public void shouldSpeedPlayerEqual10() {
+
+        Utils.isGreenLight = false;
+        Utils.maxSpeed = 10;
+        int[] speeds = {1, 7, 10};
+
+        int[] expected = {};
+        int[] actual = Utils.getSpeed(speeds);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+
+    public void shouldSpeedPlayerLimitValue9() {
+
+        Utils.isGreenLight = false;
+        Utils.maxSpeed = 10;
+        int[] speeds = {1, 9, 12};
+
+        int[] expected = {12};
+        int[] actual = Utils.getSpeed(speeds);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+
+    public void shouldSpeedPlayerLimitValue11() {
+
+        Utils.isGreenLight = false;
+        Utils.maxSpeed = 10;
+        int[] speeds = {1, 9, 11};
+
+        int[] expected = {11};
+        int[] actual = Utils.getSpeed(speeds);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+
+    public void shouldSpeedPlayer0IsGrinLightTru() {
+
+        Utils.isGreenLight = true;
+        Utils.maxSpeed = 10;
+        int[] speeds = {1, 0, 12};
+
+        int[] expected = {};
+        int[] actual = Utils.getSpeed(speeds);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+
+    public void shouldSpeedPlayer0IsGreenFalse() {
+
+        Utils.isGreenLight = false;
+        Utils.maxSpeed = 10;
+        int[] speeds = {1, 0, 12};
+
+        int[] expected = {12};
+        int[] actual = Utils.getSpeed(speeds);
 
         Assertions.assertArrayEquals(expected, actual);
 
